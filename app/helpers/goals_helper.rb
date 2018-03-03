7285ee8834ac2b04e969c956ac5805f7dd392cd7
chrissyhunt87 <chrissy.hunt@gmail.com>
2018-03-02 19:18:49 -0500

    Built generate_date_range Goal helper method

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -1,2 +1,22 @@
 module GoalsHelper
+
+  def generate_date_range(start_date, end_date, interval)
+    start_date = Date.parse(start_date)
+    end_date = Date.parse(end_date)
+    date_range = start_date..end_date
+    all_dates = []
+
+    date_range.each do |date|
+      if interval == "monthly"
+        all_dates << date if date.mday == 1
+      elsif interval == "weekly"
+        all_dates << date if date.wday == 1
+      else
+        all_dates << date
+      end
+    end
+
+    all_dates
+  end
+  
 end
